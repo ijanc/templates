70441f087263395b78bb70888bbb7de7574e4ff9
murilo ijanc <murilo@ijanc.org>
2025-11-22 11:02:01 -0300

    Add authors placeholder to automatic write author to license

--- cli/template/build.rs
+++ cli/template/build.rs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) {{ "today" | date: "%Y" }}  murilo ijanc' <murilo@ijanc.org>
+// Copyright (c) {{ "today" | date: "%Y" }} {{ authors }}
 //
 // Permission to use, copy, modify, and distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
