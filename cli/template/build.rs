d6e12aa845154a0c1543b4f84250d8a7afe72ec7
murilo ijanc <murilo@ijanc.org>
2025-11-22 10:59:06 -0300

    Add liquid filter date to generate year to licenses

--- cli/template/build.rs
+++ cli/template/build.rs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2025 murilo ijanc' <murilo@ijanc.org>
+// Copyright (c) {{ "today" | date: "%Y" }}  murilo ijanc' <murilo@ijanc.org>
 //
 // Permission to use, copy, modify, and distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
